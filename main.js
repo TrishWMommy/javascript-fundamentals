07d2598ec6de791cacb7e421574ea39aeab76ce6
Patricia <patricia.willis847@gmail.com>
2022-10-06 15:33:55 -0500

    Lesson 2 - My First Application

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('hello world');
+
+// Single Line Comment
+
+/**Multi Line 
+ * Comments
+ * 
+ */
+let variableName = 'Hello';
+
+console.log(variableName);
+
+variableName = 10;
+
+console.log(variableName);
