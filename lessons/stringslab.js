cf8d7699426c52c43dada183d3921a950a64f472
Patricia <patricia.willis847@gmail.com>
2022-10-17 23:24:23 -0500

    stringslab.js

---
+++ lessons/stringslab.js
@@ -0,0 +1,11 @@
+" This statement has empty spaces "
+/* The includes method() determines if a searched value is in a variable of string. It will return either true or false.
+/* The at method() returns the character at a specified index. It will except negetive integers (numbers) which could back from the last character in the string. This last position starts at -1 and not 0 */
+/* The trim method() removes the empty spaces from the beginning and end of the string. */
+/* toLowercase() returns the string with all characters in Lowercase */
+
+let phrase = 'Jack and Jill went up the hill to fetch a pail of water';.
+let phrase = 'Jack fell down and broke his crown and Jill came 
+tumbling after';
+console.log(phrase);
+
