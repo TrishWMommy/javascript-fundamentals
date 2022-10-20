e1f478c91df8888c7b822871914ac7dafe9cb01c
Patricia <patricia.willis847@gmail.com>
2022-10-19 23:36:33 -0500

    numberslab.js

---
+++ labs/numberslab.js
@@ -0,0 +1,20 @@
+let value1 = 34_038;
+let value2 = 45838_8734;
+let value3 = 0b011_000_101_011;
+let value4 = 0x17_4c0;
+let value5 = -895_612;
+
+console.log(value1);
+console.log(value2);
+console.log(value3);
+console.log(value4);
+console.log(value5);
+
+let Hexadecimal = 0x1D;
+let Binary = 0B11101;
+let Octal = 35;
+
+console.log(0x1D);
+console.log(0B11101);
+console.log(0o35);
+
