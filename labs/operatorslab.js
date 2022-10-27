2210f0753165904bbecdbac159ae18f5bf147a6c
Patricia <patricia.willis847@gmail.com>
2022-10-26 23:33:08 -0500

    Operators Lab

---
+++ labs/operatorslab.js
@@ -0,0 +1,36 @@
+function mathOrder() {
+    const total = (10) + 32 * (12 / 3);
+    console.log(`Math order ${total}`);
+
+    const add = 10 + 32 * 12/3;
+    console.log(`Math order ${168}`);
+
+
+} // end function();
+
+
+mathOrder();
+
+
+/* Addition */
+function exampleAdd(num2, num5) {
+    const add = num2 + num5;
+
+    console.log(`Addition ${add}`);
+
+} // end function
+
+exampleAdd();
+
+/* Comparison Operators */
+/* These evaluate to either true or false */
+function compareExample(value3, value4) {
+    const isSame = value3== value4;
+    const isOtherSame = value3 === value4;
+    const isDifferent = value3 != value4;
+
+console.log(`is same ${isSame} is other same ${isOtherSame} is different ${isDifferent}`);
+
+} // end function
+
+compareExample();
